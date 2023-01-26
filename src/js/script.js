8162b61fec7bf51c75a873c1973e2beca88d093f
CodeNoGorD <code.nogord@gmail.com>
2023-01-26 16:07:52 +0100

    derniere version 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,69 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-      $el.classList.add('is-active');
-    }
-  
-    function closeModal($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllModals() {
-      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-        closeModal($modal);
-      });
-    }
-  
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
     });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        document.location.href="./index.html";
-        // closeModal($target);
-      });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') ||
+    []
+  ).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      document.location.href = './index.html';
+      // closeModal($target);
+    });
     // });
-  
+
     // Add a keyboard event to close all modals
     document.addEventListener('keydown', (event) => {
       const e = event || window.event;
-  
-      if (e.keycode === 27) { // Escape key
+
+      if (e.keycode === 27) {
+        // Escape key
         closeAllModals();
       }
     });
   });
-  window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 0);
-  })
-  var sidenav = document.getElementById("mySidenav");
-  var openBtn = document.getElementById("openBtn");
-  var closeBtn = document.getElementById("closeBtn");
-  
+  window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+  var sidenav = document.getElementById('mySidenav');
+  var openBtn = document.getElementById('openBtn');
+  var closeBtn = document.getElementById('closeBtn');
+
   openBtn.onclick = openNav;
   closeBtn.onclick = closeNav;
-  
+
   /* Set the width of the side navigation to 250px */
   function openNav() {
-    sidenav.classList.add("active");
+    sidenav.classList.add('active');
   }
-  
+
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    sidenav.classList.remove("active");
-  }  
-  window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 0);
-  })
+    sidenav.classList.remove('active');
+  }
+});
+// window.addEventListener("scroll",function(){
+//   var header = document.querySelector("header");
+//   header.classList.toggle("sticky",window.scrollY > 0);
+// });
